772d105c335d45eccd4bbefde9dbfafaa7c3803c
JungwooSim <cscd053@gmail.com>
2022-04-07 21:14:19 +0900

    add : chapter8 - 테스트 가능한 설계

---
+++ section8/src/test/java/PaySyncTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+import payinfo.PayInfo;
+import testable.MemoryPayInfoDao;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PaySyncTest {
+    private MemoryPayInfoDao memoryDao = new MemoryPayInfoDao();
+
+    @Test
+    void allDataSaved() throws IOException {
+        PaySync paySync = new PaySync();
+        paySync.setPayInfoDao(memoryDao);
+        paySync.setFilePath("src/test/resources/c0111.csv");
+
+        paySync.sync();
+
+        List<PayInfo> savedInfos = memoryDao.getAll();
+        assertEquals(2, savedInfos.size());
+    }
+}
